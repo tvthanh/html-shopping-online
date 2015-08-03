892af76c2d9bbf3d130d47a2d3b56306ed78b98f
tvthanh <truongvanthanhagu@gmail.com>
2015-08-03 23:56:10 +0700

    Completed the layout foseven page

--- js/common.js
+++ js/common.js
@@ -4,12 +4,41 @@
 	$('.btn-bar').on('click', function() {
 		$(this).parents('header').find('.navigation .box-navi').slideToggle();
 	});
+	
+	$(".flag-controll .dropdown-menu a").on('click', function() {
+		var curAttr = $(this).find('img').attr('src');
+		var curCountry = $(this).find('span').text();
 
-	// Event show navigation with screen 489px
-	// $('.ic-search').on('click', function() {
-	// 	$(this).parents('header').find('.header-top .form-search').slideToggle();
-	// });
+		$('.flag-controll').find('img.flag-pri').attr('src', curAttr);
+		$('.flag-controll').find('span.country-pri').html(curCountry);
+	});
+	
+	var options = [];
 
+	$( '.dropdown-filter a' ).on( 'click', function( event ) {
+
+	   var $target = $( event.currentTarget ),
+	       val = $target.attr( 'data-value' ),
+	       $inp = $target.find( 'input' ),
+	       idx;
+
+	   if ( ( idx = options.indexOf( val ) ) > -1 ) {
+	      options.splice( idx, 1 );
+	      setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+	   } else {
+	      options.push( val );
+	      setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+	   }
+
+	   $( event.target ).blur();
+	      
+	   console.log( options );
+	   return false;
+	});
+
+	// hide #back-top first
+	$(".scrollTop").hide();
+	
 	// fade in #back-top
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 150) {
@@ -20,7 +49,7 @@
 	});
 
 	// scroll body to 0px on click
-	$('.scrollTop').click(function () {
+	$('.scrollTop a').click(function () {
 		$('body,html').animate({
 			scrollTop: 0
 		}, 800);
